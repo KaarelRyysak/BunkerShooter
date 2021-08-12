6db8ad3bd600321970d561e375ef82a7cbaa401e
Kaarel Ryysak <kaarel@ryysak.com>
2021-08-12 13:36:44 +0300

    Basic machinegun aiming implementation

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] private Transform destination;
+
+    private UnityEngine.AI.NavMeshAgent navMeshAgent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        if (navMeshAgent == null)
+        {
+            Debug.LogError("The navmesh agent isn't attached to " + this.gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetDestination()
     {
-        
+        if (destination != null)
+        {
+            Vector3 targetVector = destination.transform.position;
+            navMeshAgent.SetDestination(targetVector);
+        }
     }
+
 }
